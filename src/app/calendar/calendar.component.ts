a787bada85c49eb95a1ee675b9cdf4179fac1740
Alexis Corrales Perea <alexiscorralesp@gmail.com>
2020-07-27 17:54:44 -0500

    imports lodash, moment and moment-range

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as _ from 'lodash';
+import * as Moment from 'moment';
+import { extendMoment } from 'moment-range';
+const moment = extendMoment(Moment);
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
