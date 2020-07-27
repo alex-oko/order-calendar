aee01ec1a4ba94aca5e41cee3779efba51528224
Alexis Corrales Perea <alexiscorralesp@gmail.com>
2020-07-27 17:55:09 -0500

    added function 'loadInfoCalendar' for generate info calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -11,10 +11,52 @@ const moment = extendMoment(Moment);
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-
-  constructor() { }
+  objActualDate = {
+    month: moment().format('MMMM'),
+    year: moment().format('YYYY'),
+    momentFormat: moment()
+  };
+  daysWeek = ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'];
+  calendar = [];
+  constructor() {}
 
   ngOnInit(): void {
+      this.loadInfoCalendar(this.objActualDate.momentFormat);
+  }
+
+  loadInfoCalendar(actualDate): any {
+    const startWeek = moment(actualDate).startOf('month').startOf('isoWeek');
+    const endWeek = moment(actualDate).endOf('month').endOf('isoWeek');
+    const selectMonthStart = moment(actualDate).startOf('month');
+    const selectMonthEnd = moment(actualDate).endOf('month');
+
+    const range = moment.range(startWeek, endWeek);
+    const daysArray = Array.from(range.by('days'));
+    const weeksArray = Array.from(range.by('weeks'));
+
+    let objDate;
+    const array = [];
+    _.forEach(weeksArray, (w) => {
+        objDate = {
+          week: w.isoWeek(),
+          days: []
+        };
+        _.forEach(daysArray, (day) => {
+          if (w.isoWeek() === day.isoWeek()) {
+            objDate.days.push({
+              objMoment: day,
+              date: day.format('YYYY-MM-DD'),
+              week: day.isoWeek(),
+              day: day.format('DD'),
+              actualDay: moment(day.format('YYYY-MM-DD')).isSame(moment().format('YYYY-MM-DD')),
+              isActualMonth: day.isBetween(selectMonthStart, selectMonthEnd, null, '[]')
+            });
+          }
+        });
+        array.push(objDate);
+    });
+    this.calendar = array;
+    console.log(this.calendar);
   }
 
 }
