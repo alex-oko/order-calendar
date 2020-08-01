76d2c38c428dba2a0868ecbd113b151e2044a21c
Alexis Corrales Perea <alexiscorralesp@gmail.com>
2020-07-31 23:32:00 -0500

    added card of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-
+  objLogin;
+  preloaderLogin;
   constructor() { }
 
   ngOnInit(): void {
+    this.objLogin = {
+      email: '',
+      password: ''
+    };
+  }
+
+  login(): void {
+    console.log('login');
   }
 
 }
