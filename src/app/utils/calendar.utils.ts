f4df128a24a6ac633f614c1f0289d3470c54e515
Alexis Corrales Perea <alexiscorralesp@gmail.com>
2020-08-19 21:38:49 -0500

    added utils for calendar (holidays and days week)

---
+++ src/app/utils/calendar.utils.ts
@@ -0,0 +1,20 @@
+import * as _ from 'lodash';
+import * as Moment from 'moment';
+
+export const constants = {
+  daysWeek: ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
+  holidays: {
+    enero: ['01', '06'],
+    febrero: [],
+    marzo: ['23'],
+    abril: ['09', '10'],
+    mayo: ['01', '25'],
+    junio: ['15', '22', '29'],
+    julio: ['20'],
+    agosto: ['07', '17'],
+    septiembre: [],
+    octubre: ['12'],
+    noviembre: ['2', '16'],
+    diciembre: ['8', '25']
+  }
+};
